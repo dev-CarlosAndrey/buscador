728356c90774bfb7fa068f2c6dac5c5069147d41
dev-CarlosAndrey <andreybezerra.info@gmail.com>
2023-08-29 15:38:30 -0300

    three

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
+import {useState} from 'react';
 import {FiSearch} from 'react-icons/fi';
 import './style.css';
 
+
 function App() {
-  return (
-    <div className="container">
+
+const [input, setInput] = useState('')
+
+function handleSearch(){
+  alert('VALOR DO INPUT ' + input)
+
+}
+
+return (
+<div className="container">
 <h1 className="title"> Buscador CEP </h1>
 
-<div>
+<div className='containerInput'>
 
-<input
+<input 
+
 type = "text"
 placeholder="Digite seu cep ..."
+value={input}
+onChange={(e) =>setInput(e.target.value)}
 />
 
-<button className="buttonSearch">
-  <FiSearch size={25} color="#000"/>
+<button className="buttonSearch" onClick={handleSearch}>
+  <FiSearch size={25} color= " #fff " />
 </button> 
 
 </div>
 
 <main className='main'>
+
   <h2> CEP: 79003222 </h2>
+  <span> Rua Teste algum </span>
+  <span> Complemento: Algum complento </span>
+  <span> Vila Rosa </span>
+  <span> Campo Grande - MS </span>
+  
 </main>
 
-<span>Rua Teste algum</span>
-<span>Complemento: Algum complento</span>
-<span>Vila Rosa</span>
-<span>Campo Grande - MS</span>
 </div>
+
   );
-}
+  
+  }
 
 export default App;
